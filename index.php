ecc749049832d8139be661cbc2f66745b418f117
unknown <miguel.serdeira@hotmail.com>
2021-01-11 19:48:35 +0000

    Adição de bootstrap em alguns aspetos da página para ficar com melhor aspeto além de form-control disabled para que não está logged in

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ function createCommentRow($data) {
         <div class="userComment">'.$data['comment'].'</div>
         <div class="reply">
      
-        <a href="javascript:void(0)" visibility: hidden; data-commentID="'.$data['id'].'"  onclick="reply(this)">REPLY</a>
+        <button class="btn btn-secondary" href="javascript:void(0)" visibility: hidden; data-commentID="'.$data['id'].'"  onclick="reply(this)">REPLY</button>
     
         
      </div> <div class="replies">';
@@ -160,7 +160,7 @@ $numComments = $sqlNumComments->num_rows;
        
       </style>
    </head>
-   <body>
+   <body style=" background:linear-gradient(to right, #7592A2, #516395);";>
 
       <div class="modal" id="registerModal">
          <div class="modal-dialog">
@@ -198,26 +198,57 @@ $numComments = $sqlNumComments->num_rows;
             </div>
          </div>
       </div>
-       <?php
-     
-       ?>
-      <div class="container" style = "margin-top:20px;">
-      <div class="row">
-            <div class="col-md-12" align="right">
-        <?php
-        if (!$loggedIn) echo '
-                        <button class="btn btn-primary" data-toggle="modal" data-target="#registerModal">Register</button>
-                        <button class="btn btn-success" data-toggle="modal" data-target="#logInModal">Log In</button>
-                ';
-         else echo '
-                    <a href="logout.php" class="btn btn-warning">Log Out</a>
-                ';
-        ?>
-            </div>
-        </div>
-         <div class="row" style= "margin-top: 20px; margin-bottom: 20px;">
-            <div class="col-md-12">
+
+      
+      
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark border-bottom px-4">
+        <a class="navbar-brand text-white" href="#">TranslateMe</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
             <?php
+            
+            if (!$loggedIn) echo '
+           
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+              
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <button class="btn btn-success" data-toggle="modal" data-target="#logInModal">Log In</button>
+                    </li>
+                    <li class="nav-item px-2">
+                        <button class="btn btn-secondary" data-toggle="modal" data-target="#registerModal">Register</button>
+                    </li>
+              
+                    </ul>
+                    </div>
+                            
+                    ';
+            else echo '
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav">
+                    <li class="nav-item px-2">
+                     <a href="logout.php" class="btn btn-secondary">Log Out</a>
+                    </li>
+                 </ul>
+            </div>
+
+                    ';
+            ?>
+
+
+        </nav>
+        
+
+      
+</div>
+
+<div class="container">
+  <div class="row ">
+  <div class="row ">
+    <div class="colalign-self-center">
+    <?php
                if ($loggedIn == true){
                echo'<textarea class="form-control" id ="mainComment" placeholder="Add public comment" cols="30" rows="2"></textarea><br>';
            
@@ -228,18 +259,35 @@ $numComments = $sqlNumComments->num_rows;
                
                ?>
 
-            </div>
-         </div>
+    </div>
+  </div>
+  </div>
+</div>
+
+
+            
+
+
+   <div class="px-4">
          <div class="row" style= "margin-top: 20px;">
             <div class="col-md-12">
+           
                <h2><b id="numComments"> <?php echo $numComments ?> Comments</b> </h2>
                <div class="userComments">
                 
                </div>
+  
             </div>
          </div>
       </div>
-      <div class="row replyRow" style= "margin-top: 20px; margin-bottom: 20px; display:none;">
+
+
+
+
+
+
+
+      <div class="row replyRow" style= "display:none;">
             <div class="col-md-12">
             <?php
                if ($loggedIn == true){
@@ -248,11 +296,11 @@ $numComments = $sqlNumComments->num_rows;
                 } else echo '<textarea disabled class="form-control" id ="replyComment" placeholder="You need to be logged in to reply to a comment!" cols="30" rows="2"></textarea><br>';
 
               ?>
-               <button style="float:right;" class="btn-default btn" onClick="$('.replyRow').hide();">Close</button>
+               <button style="float:right;" class="btn btn-secondary" onClick="$('.replyRow').hide();">Close</button>
             </div>
          </div>
 
-
+</div>
       <script src="http://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
